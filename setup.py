7ef8f096c992d18cec593be542d76274eaa5a5a0
laiagf.96 <laiagf.96@gmail.com>
2024-10-23 22:50:36 +0900

    update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 setup(
    name='weakDetector',
-   version='0.1',
+   version='1.0',
    author='Laia Garrobe',
    author_email='lgf3@st-andrews.ac.uk',
    packages=[],
+   license="MIT",
    #long_description=open('README.txt').read(),
 )
