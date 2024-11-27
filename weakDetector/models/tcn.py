6a2149bf78ef4751b5819d99589acaa726261edb
Laia <laiagf.96@gmail.com>
2024-11-27 15:38:12 +0000

    modify tcn to return cnn weights if specified

--- weakDetector/models/tcn.py
+++ weakDetector/models/tcn.py
@@ -51,22 +51,25 @@ class TemporalBlock(nn.Module):
             self.downsample.weight.data.normal_(0, 0.01)
         
 
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.batch_norm1((out-self.mus[0])/self.sigmas[0])
+    def forward(self, x, log_weights=False):
+        out1 = self.conv1(x)
+        out = self.batch_norm1((out1-self.mus[0])/self.sigmas[0])
         out = self.comp1(out)
         out = self.relu1(out)
         out = self.dropout1(out)
 
-        out = self.conv2(out)
-        out = self.batch_norm2((out-self.mus[1])/self.sigmas[1])
+        out2 = self.conv2(out)
+        out = self.batch_norm2((out2-self.mus[1])/self.sigmas[1])
         out = self.comp2(out)
         out = self.relu2(out)
         out = self.dropout2(out) 
 
         res = x if self.downsample is None else self.downsample(x)
 
-        return self.relu(out + res)
+        if log_weights:
+            return self.relu(out + res), [out1, out2]
+        else:
+            return self.relu(out + res)
 
 
 class TemporalConvNet(nn.Module):
@@ -81,11 +84,23 @@ class TemporalConvNet(nn.Module):
             layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
                                      padding=(kernel_size-1) * dilation_size, dropout=dropout)]
 
-        self.network = nn.Sequential(*layers)
+        #self.network = nn.Sequential(*layers)
+        self.layers = layers
+    def forward(self, x, log_weights=False):
+        if log_weights:
+            weights = []
 
-    def forward(self, x):
-        return self.network(x)
-
+        for layer in self.layers:
+            if log_weights:
+                x, ws = layer(x, log_weights)
+                weights +=ws
+            else:
+                x = layer(x)
+        #return self.network(x)
+        if log_weights:
+            return x, weights
+        else:
+            return x
 
 class TCN(nn.Module):
     def __init__(self, input_size, output_size, num_channels, kernel_size, dropout):
@@ -93,10 +108,16 @@ class TCN(nn.Module):
         self.tcn = TemporalConvNet(input_size, num_channels, kernel_size=kernel_size, dropout=dropout)
         self.linear = nn.Linear(num_channels[-1], output_size)
 
-    def forward(self, inputs):
+    def forward(self, inputs, log_weights=False):
         """Inputs have to have dimension (N, C_in, L_in)"""
-
-        y1 = self.tcn(inputs)  # input should have dimension (N, C, L)
+        if log_weights:
+            y1, weights = self.tcn(inputs, log_weights)
+        else:
+            y1 = self.tcn(inputs)  # input should have dimension (N, C, L)
         o = self.linear(y1[:, :, -1])
-        return F.log_softmax(o, dim=1)
+
+        if log_weights:
+            return F.log_softmax(o, dim=1), weights               
+        else:
+            return F.log_softmax(o, dim=1)
     
