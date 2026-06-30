ead12681e369d75cca91d5356bd8180519f8e968
laiagf <laiagf.96@gmail.com>
2026-06-30 11:02:34 +0100

    Improve split functions to better separate train/val/test

--- weakDetector/utils/dm.py
+++ weakDetector/utils/dm.py
@@ -11,12 +11,12 @@ import copy
 from weakDetector.config import ROOT_DIR, SOURCES
 
 
-def random_split_df(csv_path, train_proportion = 0.7, manual_seed=0):
+def random_split_df(csv_path, proportions = [0.7, 0.15, 0.15], manual_seed=0):
 	"""Get filenames after random split of df.
 
 	Args:
 		csv_path (string): path to 
-		train_proportion (float, optional): _description_. Defaults to 0.7.
+		proportions (list, optional): Proportions of training, validation and test sets when randomly splitting. Defaults to [0.7, 0.15, 0.15].
 		manual_seed (int, optional): _description_. Defaults to 0.
 
 	Returns:
@@ -25,7 +25,8 @@ def random_split_df(csv_path, train_proportion = 0.7, manual_seed=0):
 	df = pd.read_csv(os.path.join(ROOT_DIR, csv_path)) # TODO how do we handle root dir here
 	total_size = len(df)
 	# Calculate sizes of the train and validation sets
-	train_size = int(train_proportion * total_size)
+	train_size = int(proportions[0] * total_size)
+	val_size = int(proportions[1] * total_size)
 
 	# Generate the same random indices as before
 	indices = torch.randperm(total_size, generator=torch.Generator().manual_seed(manual_seed))
@@ -34,20 +35,22 @@ def random_split_df(csv_path, train_proportion = 0.7, manual_seed=0):
 	train_indices = indices[:train_size].tolist()
 	train_files = list(set(df.File[train_indices]))
 
-	val_indices = indices[train_size:].tolist()
+	val_indices = indices[train_size:train_size+val_size].tolist()
 	val_files = list(set(df.File[val_indices]))
 
-	return train_files, val_files
+	test_indices = indices[train_size+val_size:].tolist()
+	test_files = list(set(df.File[test_indices]))
+	return train_files, val_files, test_files
 
 
 # TODO rethink this this needs to be in the class itself or something like that
-def split_sw_dataset(dataset:SpermWhaleDataset, cfg, random_prop = 0.7):
+def split_sw_dataset(dataset:SpermWhaleDataset, cfg, split_proportions = [0.7, 0.15. 0.15]):
 	"""Split SpermWhaleDataset into training and validation sets.
 
 	Args:
 		dataset (SpermWhaleDataset): Dataset to split.
 		cfg (omegaconf.dictconfig.DictConfig): Config dictionary.
-		random_prop (float, optional): Proportion of training set when randomly splitting. Defaults to 0.7.
+		proportions (list, optional): Proportions of training, validation and test sets when randomly splitting. Defaults to [0.7, 0.15, 0.15].
 
 	Raises:
 		ValueError: _description_
@@ -56,16 +59,23 @@ def split_sw_dataset(dataset:SpermWhaleDataset, cfg, random_prop = 0.7):
 		tuple: _description_
 	"""
 	if 'split' not in cfg.keys() or cfg['split']=='random':
-		split_n=int(len(dataset)*random_prop) 
-		train_set, val_set = torch.utils.data.random_split(dataset, [split_n, len(dataset)-split_n], generator=torch.Generator().manual_seed(cfg.random_state)) 
+		split_ns = [int(len(dataset)*p) for p in split_proportions]
+		split_ns[-1] = len(dataset) - sum(split_ns[:-1])  # Adjust the last split to account for rounding errors
+
+		train_set, val_set, test_set = torch.utils.data.random_split(dataset, split_ns, generator=torch.Generator().manual_seed(cfg.random_state)) 
 		df_dataset = dataset.annotations
-		df_dataset['training']=0
-		df_dataset.training[train_set.indices]=1
+		df_dataset['split']=''
+		df_dataset.split[train_set.indices]='train'
+		df_dataset.split[val_set.indices]='val'
+		df_dataset.split[test_set.indices]='test'
+
+
 
 	elif cfg['split']=='by_source':
-		train_set = dataset
-		val_set_sources = [s for s in SOURCES if s not in cfg['train_sources']]
-		print('Val sources', val_set_sources)
+		# For transferability experiments. train_set and val_set coming from train_sources, test_Set coming from the rest
+		#train_set = dataset
+		test_set_sources = [s for s in SOURCES if s not in cfg['train_sources']]
+		print('Test sources', test_set_sources)
 
 		#val_set = SpermWhaleDataset(annotations_file=cfg.annotations_file,
 		#						files_dir=dataset.files_dir,
@@ -73,30 +83,38 @@ def split_sw_dataset(dataset:SpermWhaleDataset, cfg, random_prop = 0.7):
 		#						sources=val_set_sources, min_snr=cfg.min_snr,
 		#						channels=dataset._channels) 
 		
-		val_set = copy.deepcopy(train_set)
+		#Copy train set into test set and just change annotations file
+		test_set = copy.deepcopy(train_set)
+		test_set.annotations = test_set._load_annotations(cfg.annotations_file, test_set_sources, cfg.min_snr)
 
-		val_set.annotations = val_set._load_annotations(cfg.annotations_file, val_set_sources, cfg.min_snr)
+		train_set_split = split_proportions[0]*len(dataset)
+		val_set_split = len(dataset)-train_set_split 
+		train_set, val_set = torch.utils.data.random_split(dataset, [train_set_split, val_set_split], generator=torch.Generator().manual_seed(cfg.random_state)) 
+
+		df_train_val = dataset.annotations	
+		df_train_val['split'] = 0
+		df_train_val.split[train_set.indices]='train'
+		df_train_val.split[val_set.indices]='val'		
 
 
-		df_train = train_set.annotations
-		df_val = val_set.annotations
-		df_train['training'] = 1
-		df_val['training'] = 0
-		df_dataset = pd.concat([df_train, df_val]).reset_index(drop=True)
+
+		def_test = test_set.annotations
+		df_test['split'] = 'test'
+		df_dataset = pd.concat([df_train_val, df_test]).reset_index(drop=True)
 	
 	else:
 		raise ValueError(f"Unsuported dataset split method {cfg['split']}.")
 	
-	return train_set, val_set, df_dataset
+	return train_set, val_set, test_set, df_dataset
 
 
-def split_click_dataset(dataset:ClickDataset, cfg, random_prop=0.7):
+def split_click_dataset(dataset:ClickDataset, cfg, proportions=[0.7, 0.15, 0.15]):
 	"""Split ClickDataset into training and validation sets.
 
 	Args:
 		dataset (ClickDataset): Dataset to split.
 		cfg (omegaconf.dictconfig.DictConfig): Config dictionary.
-		random_prop (float, optional): Proportion of training set when randomly splitting. Defaults to 0.7.
+		proportions (list, optional): Proportions of training, validation and test sets when randomly splitting. Defaults to [0.7, 0.15, 0.15].
 
 	Raises:
 		ValueError: _description_
@@ -105,41 +123,49 @@ def split_click_dataset(dataset:ClickDataset, cfg, random_prop=0.7):
 		tuple: _description_
 	"""
 	if 'split' not in cfg.keys() or cfg['split']=='random' or cfg['train_sources']=='all':
-		split_n=int(len(dataset)*random_prop) 
-		train_set, val_set = torch.utils.data.random_split(dataset, [split_n, len(dataset)-split_n],  generator=torch.Generator().manual_seed(cfg.random_state)) 
-		print(f"There are {len(dataset)} samples in the dataset, random split ({len(train_set)} training / {len(val_set)} validation).")
+		split_ns = [int(len(dataset)*p) for p in split_proportions]
+		split_ns[-1] = len(dataset) - sum(split_ns[:-1])  # Adjust the last split to account for rounding errors
+		train_set, val_set, test_set = torch.utils.data.random_split(dataset, split_ns,  generator=torch.Generator().manual_seed(cfg.random_state)) 
+		print(f"There are {len(dataset)} samples in the dataset, random split ({len(train_set)} training / {len(val_set)} validation) / {len(test_set)} test.")
 	
 	elif cfg.split=='by_source':
-		train_set=dataset
-		val_sources = [s for s in SOURCES if s not in cfg.train_sources]
-		val_set = ClickDataset(cfg.dataset, cfg.csv_file, cfg.tensor_dir, sources=val_sources)
-		print(f"There are {len(train_set)} samples in the training dataset (sources{cfg.train_sources}, and {len(val_set)} in the validation set (sources {val_sources})")
+		#split dataset into train and val, and then test_set comes from sources not in the dataset already
+
+		train_set_split = split_proportions[0]*len(dataset)
+		val_set_split = len(dataset)-train_set_split 
+		train_set, val_set = torch.utils.data.random_split(dataset, [train_set_split, val_set_split], generator=torch.Generator().manual_seed(cfg.random_state)) 		
+
+		test_sources = [s for s in SOURCES if s not in cfg.train_sources]
+		test_set = ClickDataset(cfg.dataset, cfg.csv_file, cfg.tensor_dir, sources=test_sources)
+		print(f"There are {len(train_set)} samples in the training dataset / {len(val_set)} in the val_set (sources{cfg.train_sources}, and {len(test_set)} in the test set (sources {test_sources})")
 	
 	elif cfg.split=='by_tcn_4':
-		train_files, val_files = random_split_df('files/datasets/4mindatasetB_LaiasVersion.csv', manual_seed=cfg.random_state)
+		train_files, val_files, test_files = random_split_df('files/datasets/4mindataset.csv', proportions=proportions, manual_seed=cfg.random_state)
 		train_set =  ClickDataset(cfg.dataset, cfg.csv_file, cfg.tensor_dir, sources=cfg.train_sources, split_files=train_files)
 		val_set = ClickDataset(cfg.dataset, cfg.csv_file, cfg.tensor_dir, sources=cfg.train_sources, split_files=val_files)
-		print(f"There are {len(train_set)} samples in the training dataset  and {len(val_set)}. Split made according to random split on 4min dataset B")
+		test_set = ClickDataset(cfg.dataset, cfg.csv_file, cfg.tensor_dir, sources=cfg.train_sources, split_files=test_files)
+		print(f"There are {len(train_set)} samples in the training dataset, {len(val_set)} in the val set and {len(test_set)} in the test set. Split made according to random split on 4min dataset")
 
 	elif cfg.split=='by_tcn_30':
-		print('Splitting by tcn using 30secC_LaiasVersion')
-		train_files, val_files = random_split_df('files/datasets/30secdatasetC_LaiasVersion.csv', manual_seed=cfg.random_state)
+		print('Splitting by tcn using 30sec')
+		train_files, val_files, test_files = random_split_df('files/datasets/30secdataset.csv', manual_seed=cfg.random_state)
 		train_set =  ClickDataset(cfg.dataset, cfg.csv_file, cfg.tensor_dir, sources=cfg.train_sources, split_files=train_files)
 		val_set = ClickDataset(cfg.dataset, cfg.csv_file, cfg.tensor_dir, sources=cfg.train_sources, split_files=val_files)
-		print(f"There are {len(train_set)} samples in the training dataset  and {len(val_set)}. Split made according to random split on 30 sec dataset C.")
+		test_set = ClickDataset(cfg.dataset, cfg.csv_file, cfg.tensor_dir, sources=cfg.train_sources, split_files=test_files)
+		print(f"There are {len(train_set)} samples in the training dataset, {len(val_set)} in the val set and {len(test_set)} in the test set. Split made according to random split on 30sec dataset")
 
 	else:
 		raise ValueError(f"Unsupported dataset split: {cfg.split}")
 	
-	return train_set, val_set
+	return train_set, val_set, test_set
 
-def split_dataset(dataset, cfg, random_prop=0.85):   
+def split_dataset(dataset, cfg, proportions=[0.7, 0.15, 0.15]):   
 	"""Split Dataset into training and validation sets.
 
 	Args:
 		dataset (Dataset): Dataset to split.
 		cfg (omegaconf.dictconfig.DictConfig): Config dictionary.
-		random_prop (float, optional): Proportion of training set when randomly splitting. Defaults to 0.7.
+		proportions (list, optional): Proportions of training, validation and test sets when randomly splitting. Defaults to [0.7, 0.15, 0.15].
 
 	Raises:
 		ValueError: _description_
@@ -148,8 +174,8 @@ def split_dataset(dataset, cfg, random_prop=0.85):
 		tuple: _description_
 	"""
 	if isinstance(dataset, SpermWhaleDataset):
-		return split_sw_dataset(dataset, cfg, random_prop)
+		return split_sw_dataset(dataset, cfg, proportions)
 	elif isinstance(dataset, ClickDataset):
-		return split_click_dataset(dataset, cfg, random_prop)
+		return split_click_dataset(dataset, cfg, proportions)
 	else: 
 		raise TypeError(f'{type(dataset)} not valid for dataset to split.')
